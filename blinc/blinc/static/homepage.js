e477ed4846a7e6971b218d5680bff25636d1d993
Eric Blincow <eb1@fastmail.fm>
2015-03-03 11:14:41 -0500

    Minified js and css

--- blinc/blinc/static/homepage.js
+++ blinc/blinc/static/homepage.js
@@ -1,10 +1,9 @@
+// show and hide page elements
 function showAbout() {
     $('li.selected').removeClass('selected');
     $('li.aboutlink').addClass('selected');
     $('.content').hide();
     $('#about').show();
-    console.log("\nand you can sit and think about all those things");
-    console.log("\nwhile sipping on whiskey and coke.");
 }
 function showLogin() {
     $('li.selected').removeClass('selected');
@@ -12,7 +11,6 @@ function showLogin() {
     $('.content').hide();
     $('#about').hide();
     $('#login').show();
-    console.log("\nThis login feature sees about as much use as a snow-plough in Ecuador.");
 }
 
 function showContact() {
@@ -33,27 +31,63 @@ function showPastWork() {
 }
 
 
-$(document).ready(function () {
-    console.log("\nSometimes you say things in life you don't mean.");
-    console.log("\nand ... it turns out there's no way to take them back.");
+function homepageJS() {
+    // bind click handlers for links
+    // to show and hide html
+    $(document).ready(function () {
+        console.log("\nSometimes you say things in life you don't mean.");
+        console.log("\nand ... it turns out there's no way to take them back.");
+
+        $(".aboutlink").click(function (e) {
+            e.preventDefault();
+            showAbout();
+        });
+        $(".contactlink").click(function (e) {
+            e.preventDefault();
+            showContact();
+        });
+        $(".loginlink").click(function (e) {
+            e.preventDefault();
+            showLogin();
+        });
+        $(".pastworklink").click(function (e) {
+            e.preventDefault();
+            showPastWork();
+        });
 
 
-    $(".aboutlink").click(function (e) {
-        e.preventDefault();
-        showAbout();
     });
-    $(".contactlink").click(function (e) {
-        e.preventDefault();
-        showContact();
-    });
-    $(".loginlink").click(function (e) {
-        e.preventDefault();
-        showLogin();
-    });
-    $(".pastworklink").click(function (e) {
-        e.preventDefault();
-        showPastWork();
-    });
+}
 
 
-});
+// loading and deferring
+function defer(method) {
+    // check for jquery, when loaded call method
+    if (window.$)
+        method();
+    else
+        setTimeout(function () {
+            defer(method);
+        }, 50);
+}
+
+function downloadJqueryAtOnload() {
+    // defer certain images and download jquery
+    var imgDefer = document.getElementsByTagName('img');
+    for (var i = 0; i < imgDefer.length; i++) {
+        if (imgDefer[i].getAttribute('data-src')) {
+            imgDefer[i].setAttribute('src', imgDefer[i].getAttribute('data-src'));
+        }
+    }
+    var element2 = document.createElement("script");
+    element2.src = '/static/jquery-2.1.3.min.js';
+    document.body.appendChild(element2);
+    defer(homepageJS);
+}
+
+// onload, get jquery and switch image attributes
+if (window.addEventListener)
+    window.addEventListener("load", downloadJqueryAtOnload, false);
+else if (window.attachEvent)
+    window.attachEvent("onload", downloadJqueryAtOnload);
+else window.onload = downloadJqueryAtOnload;
